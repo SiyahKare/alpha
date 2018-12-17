59d321d7f4fedca21c672aa4fbd10fc375fffbb4
Mono Cidar <onurmutlu@gmail.com>
2018-12-17 04:34:57 +0300

    alpha0434

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>SiyahKare [alpha]</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -63,7 +63,7 @@
             }
         </style>
     </head>
-    <body>
+    <body background="/assets/image/construct.jpg">
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    SiyahKare
                 </div>
 
                 <div class="links">
