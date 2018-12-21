354b6f6153751c6de4c21936aaff47846c99dadc
Mono Cidar <onurmutlu@gmail.com>
2018-12-22 02:03:42 +0300

    forge1
    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,12 +85,12 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://www.siyahkare.com/docs">Documentation</a>
+                    <a href="https://www.siyahkare.com/">Solutions</a>
+                    <a href="https://www.siyahkare.net/">Productions</a>
+                    <a href="https://org.siyahkare.com/">Decentralized</a>
+                    <a href="https://www.siyahkare.site/">Blockchain</a>
+                    <a href="https://github.com/SiyahKare/SiyahKare">Source Code</a>
                 </div>
             </div>
         </div>
